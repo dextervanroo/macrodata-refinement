6865b6495fbe3a64796202973f03fcc7013085eb
dextervanroo <felippe.vanroo@gmail.com>
2026-05-01 23:54:59 -0300

    Add functions for data reading and processing

---
+++ data_refiner.py
@@ -0,0 +1,19 @@
+def read_csv(file):
+    pass
+
+def read_txt(file):
+    pass
+
+def read_mdr(file):
+    pass
+
+def process_data_refinement():
+    pass
+
+def sum_data_refinement(data):
+    pass
+
+if __name__ == "__main__":
+    refined_data = process_data_refinement()
+    sum_value = sum_data_refinement(refined_data)
+    print(sum_value)
